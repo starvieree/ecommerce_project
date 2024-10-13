54464ade83e8b69d72abea1af5e35417de8f3da5
starvieree <ainurreza90788@gmail.com>
2024-10-13 12:24:08 -0700

    commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,9 +10,13 @@
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
 
+        @livewire('partials.navbar')
+
         <main>
             {{ $slot }}
         </main>
+
+        @livewire('partials.footer')
 
         @livewireScripts
     </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,45 @@
 <?php
 
+use App\Livewire\CartPage;
 use App\Livewire\HomePage;
+use App\Livewire\CancelPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\ProductDetailPage;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\Auth\ForgotPasswordPage;
 
 Route::get('/', HomePage::class);
+
+Route::get('/categories', CategoriesPage::class);
+
+Route::get('/products', ProductsPage::class);
+
+Route::get('/cart', CartPage::class);
+
+Route::get('/products/{product}', ProductDetailPage::class);
+
+Route::get('/checkout', CheckoutPage::class);
+
+Route::get('/my-orders', MyOrdersPage::class);
+
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+
+Route::get('/login', LoginPage::class);
+
+Route::get('/register', RegisterPage::class);
+
+Route::get('/forgot', ForgotPasswordPage::class);
+
+Route::get('/reset', ResetPasswordPage::class);
+
+Route::get('/success', SuccessPage::class);
+
+Route::get('/cancel', CancelPage::class);
