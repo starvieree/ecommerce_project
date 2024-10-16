92830e2b69a298d511997a5d4e8bf43d7b98dc13
starvieree <ainurreza90788@gmail.com>
2024-10-15 21:17:58 -0700

    commit

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use App\Models\Category;
+use Livewire\Attributes\Title;
+
+#[Title('Home Page - DCodeMania')]
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', [
+            'brands' => $brands,
+            'categories' => $categories
+        ]);
     }
 }
