e231fe94f950e02c53b69bb304d51876f6fb1e44
starvieree <ainurreza90788@gmail.com>
2024-10-17 20:02:19 -0700

    commit

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 
 #[Title('Products Page - DCodeMania')]
@@ -16,11 +17,46 @@ class ProductsPage extends Component
 
     use WithPagination;
 
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 20000000;
+
     public function render()
     {
         $categories = Category::where('is_active', 1)->get(['id', 'name', 'slug']);
         $brands = Brand::where('is_active', 1)->get(['id', 'name', 'slug']);
         $productQuery = Product::query()->where('is_active', 1);
+
+        if (!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if (!empty($this->selected_brands)) {
+            $productQuery->whereIn('brands_id', $this->selected_brands);
+        }
+
+        if ($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+
+        if ($this->on_sale) {
+            $productQuery->where('on_sale', 1);
+        }
+
+        if ($this->price_range) {
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
 
         return view('livewire.products-page', [
             'categories' => $categories,
--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -11,7 +11,7 @@
                                 @foreach ($categories as $category)
                                 <li class="mb-4" wire:key="{{ $category->id }}">
                                     <label for="{{ $category->slug }}" class="flex items-center dark:text-gray-400 ">
-                                        <input type="checkbox" id="{{ $category->slug }}" value="{{ $category->id }}" class="w-4 h-4 mr-2">
+                                        <input type="checkbox" wire:model.live="selected_categories" id="{{ $category->slug }}" value="{{ $category->id }}" class="w-4 h-4 mr-2">
                                         <span class="text-lg">{{ $category->name }}</span>
                                     </label>
                                 </li>
@@ -26,7 +26,7 @@
                                 @foreach ($brands as $brand)
                                 <li class="mb-4" wire:key="{{ $brand->id }}">
                                     <label for="{{ $brand->slug }}" class="flex items-center dark:text-gray-300">
-                                        <input type="checkbox" id="{{ $brand->slug }}" value="{{ $brand->id }}" class="w-4 h-4 mr-2">
+                                        <input type="checkbox" wire:model.live="selected_brands" id="{{ $brand->slug }}" value="{{ $brand->id }}" class="w-4 h-4 mr-2">
                                         <span class="text-lg dark:text-gray-400">{{ $brand->name }}</span>
                                     </label>
                                 </li>
@@ -38,14 +38,14 @@
                             <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                             <ul>
                                 <li class="mb-4">
-                                    <label for="" class="flex items-center dark:text-gray-300">
-                                        <input type="checkbox" class="w-4 h-4 mr-2">
-                                        <span class="text-lg dark:text-gray-400">In Stock</span>
+                                    <label for="featured" class="flex items-center dark:text-gray-300">
+                                        <input type="checkbox" id="featured" wire:model.live="featured" value="1" class="w-4 h-4 mr-2">
+                                        <span class="text-lg dark:text-gray-400">Featured Products</span>
                                     </label>
                                 </li>
                                 <li class="mb-4">
-                                    <label for="" class="flex items-center dark:text-gray-300">
-                                        <input type="checkbox" class="w-4 h-4 mr-2">
+                                    <label for="on_sale" class="flex items-center dark:text-gray-300">
+                                        <input type="checkbox" id="on_sale" wire:model.live="on_sale" value="1" class="w-4 h-4 mr-2">
                                         <span class="text-lg dark:text-gray-400">On Sale</span>
                                     </label>
                                 </li>
@@ -56,12 +56,13 @@
                             <h2 class="text-2xl font-bold dark:text-gray-400">Price</h2>
                             <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
                             <div>
-                                <input type="range"
+                                <div class="font-semibold">{{ Number::currency($price_range, 'INR') }}</div>
+                                <input type="range" wire:model.live="price_range"
                                     class="w-full h-1 mb-4 bg-blue-100 rounded appearance-none cursor-pointer"
-                                    max="500000" value="100000" step="100000">
+                                    max="100000000" value="20000000" step="1000000">
                                 <div class="flex justify-between ">
-                                    <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 1000</span>
-                                    <span class="inline-block text-lg font-bold text-blue-400 ">&#8377; 500000</span>
+                                    <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(1000000, 'INR') }}</span>
+                                    <span class="inline-block text-lg font-bold text-blue-400 ">{{ Number::currency(100000000, 'INR') }}</span>
                                 </div>
                             </div>
                         </div>
