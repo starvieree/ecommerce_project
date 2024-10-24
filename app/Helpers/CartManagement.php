175923c1789db34f3ac066bb8205e89a1bccd245
starvieree <ainurreza90788@gmail.com>
2024-10-24 13:32:15 -0700

    commit

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -41,12 +41,46 @@ class CartManagement
         return count($cart_items);
     }
 
+    // Add item to cart with Qty
+    static public function addItemToCartWithQty($product_id, $qty = 1) {
+        $cart_items = self::getCartItemsFromCookie();
+
+        $existing_item = null;
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                $existing_item = $key;
+                break;
+            }
+        }
+
+        if ($existing_item !== null) {
+            $cart_items[$existing_item]['quantity'] = $qty;
+            $cart_items[$existing_item]['total_amount'] = $cart_items[$existing_item]['quantity'] * $cart_items[$existing_item]['unit_amount'];
+        } else {
+            $product = Product::where('id', $product_id)->first(['id', 'name', 'price', 'images']);
+            if ($product) {
+                $cart_items[] = [
+                    'product_id' => $product_id,
+                    'name' => $product->name,
+                    'images' => $product->images[0],
+                    'quantity' => $qty,
+                    'unit_amount' => $product->price,
+                    'total_amount' => $product->price
+                ];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+        return count($cart_items);
+    }
+
     // Remove item from cart
     static public function removeCartItem($product_id) {
         $cart_items = self::getCartItemsFromCookie();
 
         foreach ($cart_items as $key => $item) {
-            if ($item['product_id '] == $product_id) {
+            if ($item['product_id'] == $product_id) {
                 unset($cart_items[$key]);
             }
         }
--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -3,9 +3,42 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 
 class CartPage extends Component
 {
+    #[Title('Cart - DCodeMania')]
+
+    public $cart_items = [];
+    public $grand_total;
+
+    public function mount()
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeItem($product_id)
+    {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+
+    public function increaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::incrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function decreaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::decrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
     public function render()
     {
         return view('livewire.cart-page');
